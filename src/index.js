5e00d9a62bd94e234349eba1fd7aed976a5428bc
Omnia <omniaeid314@gmail.com>
2024-07-02 22:04:41 +0300

    I just added a comment to the App.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ root.render(
     </Router>
   </React.StrictMode>
 );
+// for trying
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
